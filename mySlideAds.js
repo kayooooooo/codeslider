4355cb88fd03e157fbc1e3e85d056819125917d0
kayooooooo <108146877+kayooooooo@users.noreply.github.com>
2022-09-23 11:54:23 -0300

    Update mySlideAds.js

--- mySlideAds.js
+++ mySlideAds.js
@@ -1,15 +1,14 @@
-
 var myIndex = 0;
 carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName(&quot;mySlides0&quot;);
-  for (i = 0; i &lt; x.length; i++) {
-    x[i].style.display = &quot;none&quot;;  
+  var x = document.getElementsByClassName("mySlides0");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
   }
   myIndex++;
-  if (myIndex &gt; x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = &quot;block&quot;;  
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 3000); // Change image every 2 seconds
 }
