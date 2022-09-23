9b84b0e97fc5a2ed1eac4543c2d47c53020a843b
kayooooooo <108146877+kayooooooo@users.noreply.github.com>
2022-09-23 12:09:27 -0300

    Update mySlideAds.js

--- mySlideAds.js
+++ mySlideAds.js
@@ -1,3 +1,4 @@
+<script>
 var myIndex = 0;
 carousel();
 
@@ -12,3 +13,4 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 3000); // Change image every 2 seconds
 }
+</script>
